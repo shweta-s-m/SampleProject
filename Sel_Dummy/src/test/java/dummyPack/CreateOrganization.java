f0a96d8a7aecd79932058944a508e330d4a9a621
Admin <Admin@DESKTOP-9P7INMV.mshome.net>
2024-07-27 14:36:31 +0530

    add

---
+++ Sel_Dummy/src/test/java/dummyPack/CreateOrganization.java
@@ -0,0 +1,16 @@
+package dummyPack;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.mysql.jdbc.Driver;
+
+public class CreateOrganization {
+
+	
+    @Test
+    public void add() {
+    	System.out.println("Pass");
+    }
+}
---
+++ Sel_Dummy/src/test/java/dummyPack/Dummyclass.java
@@ -0,0 +1,14 @@
+package dummyPack;
+
+import org.testng.annotations.Test;
+
+public class Dummyclass {
+
+	@Test
+	public void Dummymodel ()
+	{
+		System.out.println("sfjsf");
+		System.out.println("jhgjwd");
+		System.out.println("jhbsjm");
+	}
+}
